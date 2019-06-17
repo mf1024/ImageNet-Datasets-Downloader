0ddde4123d1c520c12f9d09c016dbe4e2398f28a
Martins <martins.frolovs@gmail.com>
2019-06-17 14:49:46 +0300

    10x speed improvement with multiprocessing

--- downloader.py
+++ downloader.py
@@ -7,6 +7,8 @@ import time
 import logging
 import csv
 
+from multiprocessing import Pool, Process, Value, Lock
+
 from requests.exceptions import ConnectionError, ReadTimeout, TooManyRedirects, MissingSchema, InvalidURL
 
 parser = argparse.ArgumentParser(description='ImageNet image scraper')
@@ -17,6 +19,8 @@ parser.add_argument('-data_root', default='' , type=str)
 parser.add_argument('-use_class_list', default=False,type=lambda x: (str(x).lower() == 'true'))
 parser.add_argument('-class_list', default=[], nargs='*')
 parser.add_argument('-debug', default=False,type=lambda x: (str(x).lower() == 'true'))
+
+parser.add_argument('-multiprocessing_workers', default = 16, type=int)
 
 args, args_other = parser.parse_known_args()
 
@@ -101,6 +105,40 @@ def add_debug_csv_row(row):
         csv_writer = csv.writer(csv_f, delimiter=",")
         csv_writer.writerow(row)
 
+class MultiStats():
+    def __init__(self):
+
+        self.lock = Lock()
+
+        self.stats = dict(
+            all=dict(
+                tried=Value('d', 0),
+                success=Value('d',0),
+                time_spent=Value('d',0),
+            ),
+            is_flickr=dict(
+                tried=Value('d', 0),
+                success=Value('d',0),
+                time_spent=Value('d',0),
+            ),
+            not_flickr=dict(
+                tried=Value('d', 0),
+                success=Value('d', 0),
+                time_spent=Value('d', 0),
+            )
+        )
+    def inc(self, cls, stat, val):
+        with self.lock:
+            self.stats[cls][stat].value += val
+
+    def get(self, cls, stat):
+        with self.lock:
+            ret = self.stats[cls][stat].value
+        return ret
+
+multi_stats = MultiStats()
+
+
 if args.debug:
     row = [
         "all_tried",
@@ -117,29 +155,160 @@ if args.debug:
 
 def add_stats_to_debug_csv():
     row = [
-        scraping_stats['all']['tried'],
-        scraping_stats['all']['success'],
-        scraping_stats['all']['time_spent'],
-        scraping_stats['is_flickr']['tried'],
-        scraping_stats['is_flickr']['success'],
-        scraping_stats['is_flickr']['time_spent'],
-        scraping_stats['not_flickr']['tried'],
-        scraping_stats['not_flickr']['success'],
-        scraping_stats['not_flickr']['time_spent']
+        multi_stats.get('all', 'tried'),
+        multi_stats.get('all', 'success'),
+        multi_stats.get('all', 'time_spent'),
+        multi_stats.get('is_flickr', 'tried'),
+        multi_stats.get('is_flickr', 'success'),
+        multi_stats.get('is_flickr', 'time_spent'),
+        multi_stats.get('not_flickr', 'tried'),
+        multi_stats.get('not_flickr', 'success'),
+        multi_stats.get('not_flickr', 'time_spent'),
     ]
     add_debug_csv_row(row)
 
 def print_stats(cls, print_func):
-    if scraping_stats[cls]["tried"] > 0:
-        print_func(f'{100.0 * scraping_stats[cls]["success"]/scraping_stats[cls]["tried"]}% success rate for {cls} urls ')
-    if scraping_stats[cls]["success"] > 0:
-        print_func(f'{scraping_stats[cls]["time_spent"] / scraping_stats[cls]["success"]} seconds spent per {cls} succesful image download')
 
-url_tries = 0
+    actual_all_time_spent = time.time() - scraping_t_start.value
+    processes_all_time_spent = multi_stats.get('all', 'time_spent')
+
+    if processes_all_time_spent == 0:
+        actual_processes_ratio = 1.0
+    else:
+        actual_processes_ratio = actual_all_time_spent / processes_all_time_spent
+
+    #print(f"actual all time: {actual_all_time_spent} proc all time {processes_all_time_spent}")
+
+    print_func(f'STATS For class {cls}:')
+    print_func(f' tried {multi_stats.get(cls, "tried")} urls with'
+               f' {multi_stats.get(cls, "success")} successes')
+
+    if multi_stats.get(cls, "tried") > 0:
+        print_func(f'{100.0 * multi_stats.get(cls, "success")/multi_stats.get(cls, "tried")}% success rate for {cls} urls ')
+    if multi_stats.get(cls, "success") > 0:
+        print_func(f'{multi_stats.get(cls,"time_spent") * actual_processes_ratio / multi_stats.get(cls,"success")} seconds spent per {cls} succesful image download')
+
+
+
+lock = Lock()
+url_tries = Value('d', 0)
+scraping_t_start = Value('d', time.time())
+class_folder = ''
+class_images = Value('d', 0)
+
+def get_image(img_url):
+
+    #print(f'Processing {img_url}')
+
+    #time.sleep(3)
+
+    if len(img_url) <= 1:
+        return
+
+
+    cls_imgs = 0
+    with lock:
+        cls_imgs = class_images.value
+
+    if cls_imgs >= args.images_per_class:
+        return
+
+    logging.debug(img_url)
+
+    cls = ''
+
+    if 'flickr' in img_url:
+        cls = 'is_flickr'
+    else:
+        cls = 'not_flickr'
+        if args.scrape_only_flickr:
+            return
+
+    t_start = time.time()
+
+    def finish(status):
+        t_spent = time.time() - t_start
+        multi_stats.inc(cls, 'time_spent', t_spent)
+        multi_stats.inc('all', 'time_spent', t_spent)
+
+        multi_stats.inc(cls,'tried', 1)
+        multi_stats.inc('all', 'tried', 1)
+
+        if status == 'success':
+            multi_stats.inc(cls,'success', 1)
+            multi_stats.inc('all', 'success', 1)
+
+        elif status == 'failure':
+            pass
+        else:
+            logging.error(f'No such status {status}!!')
+            exit()
+        return
+
+
+    with lock:
+        url_tries.value += 1
+        if url_tries.value % 250 == 0:
+            print(f'\nScraping stats:')
+            print_stats('is_flickr', print)
+            print_stats('not_flickr', print)
+            print_stats('all', print)
+            if args.debug:
+                add_stats_to_debug_csv()
+
+    try:
+        img_resp = requests.get(img_url, timeout = 1)
+    except ConnectionError:
+        logging.debug(f"Connection Error for url {img_url}")
+        return finish('failure')
+    except ReadTimeout:
+        logging.debug(f"Read Timeout for url {img_url}")
+        return finish('failure')
+    except TooManyRedirects:
+        logging.debug(f"Too many redirects {img_url}")
+        return finish('failure')
+    except MissingSchema:
+        return finish('failure')
+    except InvalidURL:
+        return finish('failure')
+
+    if not 'content-type' in img_resp.headers:
+        return finish('failure')
+
+    if not 'image' in img_resp.headers['content-type']:
+        logging.debug("Not an image")
+        return finish('failure')
+
+    if (len(img_resp.content) < 1000):
+        return finish('failure')
+
+    logging.debug(img_resp.headers['content-type'])
+    logging.debug(f'image size {len(img_resp.content)}')
+
+    img_name = img_url.split('/')[-1]
+    img_name = img_name.split("?")[0]
+
+    if (len(img_name) <= 1):
+        return finish('failure')
+
+    img_file_path = os.path.join(class_folder, img_name)
+    logging.debug(f'Saving image in {img_file_path}')
+
+    with open(img_file_path, 'wb') as img_f:
+        img_f.write(img_resp.content)
+
+        with lock:
+            class_images.value += 1
+
+        logging.debug(f'Scraping stats')
+        print_stats('is_flickr', logging.debug)
+        print_stats('not_flickr', logging.debug)
+        print_stats('all', logging.debug)
+
+        return finish('success')
+
 
 for class_wnid in classes_to_scrape:
-
-    class_images = 0
 
     class_name = class_info_dict[class_wnid]["class_name"]
     print(f'Scraping images for class \"{class_name}\"')
@@ -152,96 +321,11 @@ for class_wnid in classes_to_scrape:
     if not os.path.exists(class_folder):
         os.mkdir(class_folder)
 
-    t_last = time.time()
-    cls=''
+    class_images.value = 0
 
-    for img_url in resp.content.splitlines():
+    urls = [url.decode('utf-8') for url in resp.content.splitlines()]
 
-        if len(img_url) <= 1:
-            continue
-
-        url_tries += 1
-
-        if url_tries % 250 == 0:
-            print(f'\nScraping stats {scraping_stats}')
-            print_stats('is_flickr', print)
-            print_stats('not_flickr', print)
-            print_stats('all', print)
-            if args.debug:
-                add_stats_to_debug_csv()
-
-        if cls:
-            t_cur = time.time()
-            t_spent = t_cur - t_last
-
-            scraping_stats[cls]['time_spent'] += t_spent
-            scraping_stats['all']['time_spent'] += t_spent
-
-            t_last = t_cur
-
-        if 'flickr' in img_url.decode('utf-8'):
-            cls = 'is_flickr'
-        else:
-            cls = 'not_flickr'
-            if args.scrape_only_flickr:
-                continue
-
-        logging.debug(img_url)
-        scraping_stats[cls]['tried'] += 1
-        scraping_stats['all']['tried'] += 1
-
-
-        try:
-            img_resp = requests.get(img_url.decode('utf-8'), timeout = 1)
-        except ConnectionError:
-            logging.debug("Connection Error")
-            continue
-        except ReadTimeout:
-            logging.debug("Read Timeout")
-            continue
-        except TooManyRedirects:
-            logging.debug("Too many redirects")
-            continue
-        except MissingSchema:
-            continue
-        except InvalidURL:
-            continue
-
-        if not 'content-type' in img_resp.headers:
-            continue
-
-        if not 'image' in img_resp.headers['content-type']:
-            logging.debug("Not an image")
-            continue
-
-        if (len(img_resp.content) < 1000):
-            continue
-
-
-        logging.debug(img_resp.headers['content-type'])
-        logging.debug(f'image size {len(img_resp.content)}')
-
-        img_name = img_url.decode('utf-8').split('/')[-1]
-        img_name = img_name.split("?")[0]
-
-        if (len(img_name) <= 1):
-            continue
-
-        img_file_path = os.path.join(class_folder, img_name)
-
-        logging.debug(f'Saving image in {img_file_path}')
-
-        with open(img_file_path, 'wb') as img_f:
-            img_f.write(img_resp.content)
-            class_images += 1
-            scraping_stats[cls]['success'] += 1
-            scraping_stats['all']['success'] += 1
-
-
-        logging.debug(f'Scraping stats {scraping_stats}')
-        print_stats('is_flickr', logging.debug)
-        print_stats('not_flickr', logging.debug)
-        print_stats('all', logging.debug)
-
-        if class_images == args.images_per_class:
-            break
+    #for url in  urls:
+    #    get_image(url)
+    with Pool(processes=args.multiprocessing_workers) as p:
+        p.map(get_image,urls)
